935cbd989752b2caa7fa6a2904c490a053df4b95
GitHub Action <action@github.com>
2025-11-30 19:59:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/lux44/zstdcompressiontakehome/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/lux44/zstdcompressiontakehome/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.lux44.zstdcompressiontakehome.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/lux44/zstdcompressiontakehome/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/lux44/zstdcompressiontakehome/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.lux44.zstdcompressiontakehome.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.lux44.zstdcompressiontakehome.MyBundle
+import com.github.lux44.zstdcompressiontakehome.services.MyProjectService
 import javax.swing.JButton
 
 
