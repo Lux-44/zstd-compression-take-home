263e8d936c05ca7e064ad004f91ecb9e46626595
Felix Poschenrieder <ge35giz@mytum.de>
2025-12-01 14:53:04 +0100

    Added file refresh after compression and better errors

--- src/main/kotlin/actions/CompressOpenFileAction.kt
+++ src/main/kotlin/actions/CompressOpenFileAction.kt
@@ -1,30 +1,44 @@
 package actions
 
+// Import for zstd
+import com.github.luben.zstd.Zstd
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
 import java.io.File
-// Import for zstd
-import com.github.luben.zstd.Zstd
 
 
-class CompressOpenFileAction : AnAction(){
+class CompressOpenFileAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
-        val currentFile = event.getData(CommonDataKeys.VIRTUAL_FILE)?:return
+        // If no file is currently open, do nothing
+        val currentFile = event.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
         compressFile(currentFile)
     }
-    private fun compressFile(currentFile: VirtualFile){
+
+    private fun compressFile(currentFile: VirtualFile) {
         try {
             val file = File(currentFile.path)
             val fileBytes = file.readBytes()
             // Compression Level 3 as it is default
-            val compBytes = Zstd.compress(fileBytes,3)
-            val compFile=File(file.absolutePath+".zst")
+            val compBytes = Zstd.compress(fileBytes, 3)
+            // Write compressed file to same location as input file
+            val compFile = File(file.absolutePath + ".zst")
             compFile.writeBytes(compBytes)
-        }catch (e: Exception){
-            Messages.showMessageDialog("Could not compress currently open file!","Compression Error",Messages.getErrorIcon())
+            // Refresh to make result visible faster
+            ApplicationManager.getApplication().invokeLater {
+                LocalFileSystem.getInstance().refreshAndFindFileByPath(compFile.absolutePath)
+            }
+        } catch (e: Exception) {
+            Messages.showMessageDialog(
+                "Could not compress currently open file!: "+e.message,
+                "Compression Error",
+                Messages.getErrorIcon()
+            )
         }
+
     }
 }
