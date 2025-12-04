0d48dd2795c70cce2f06b6859f2d78b8be47917d
Felix Poschenrieder <ge35giz@mytum.de>
2025-12-04 08:44:30 +0100

    Rewritten to use kotlin coroutines

--- src/main/kotlin/actions/CompressOpenFileAction.kt
+++ src/main/kotlin/actions/CompressOpenFileAction.kt
@@ -6,10 +6,18 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.ide.progress.withBackgroundProgress
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 
 
@@ -22,12 +30,13 @@ class CompressOpenFileAction : AnAction() {
     override fun update(e: AnActionEvent) {
         val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE)
         // Only files can be compressed
-        e.presentation.isEnabledAndVisible = virtualFile != null && !virtualFile.isDirectory
+        e.presentation.isEnabledAndVisible = virtualFile != null && !virtualFile.isDirectory && virtualFile.isInLocalFileSystem
     }
 
     override fun actionPerformed(event: AnActionEvent) {
         // If no file is currently open, do nothing
         val currentFile = event.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+        val project = event.project ?: return
         // Only compress file if it exists in the local file system
         if (!currentFile.isInLocalFileSystem) {
             Messages.showMessageDialog(
@@ -36,18 +45,33 @@ class CompressOpenFileAction : AnAction() {
                 Messages.getErrorIcon()
             )
         }
-        try {
-            compressFile(currentFile)
-        } catch (e: Exception) {
-            Messages.showMessageDialog(
-                "Could not compress currently open file!: " + e.message,
-                "Compression Error",
-                Messages.getErrorIcon()
-            )
+        project.service<FileCompressionService>().compressFile(currentFile)
+    }
+}
+
+@Service(Service.Level.PROJECT)
+class FileCompressionService(private val project: Project, private val coroutineScope: CoroutineScope) {
+    fun compressFile(currentFile: VirtualFile) {
+        coroutineScope.launch {
+            // Attempt compression
+            try {
+                withBackgroundProgress(project, "Compressing open file", cancellable = true) {
+                    compressFileInternal(currentFile)
+                }
+            } catch (e: Exception) {
+                // Dispatch error message onto EDT
+                withContext(Dispatchers.EDT){
+                    Messages.showMessageDialog(
+                        "Could not compress currently open file!: " + e.message,
+                        "Compression Error",
+                        Messages.getErrorIcon()
+                    )
+                }
+            }
         }
     }
 
-    private fun compressFile(currentFile: VirtualFile) {
+    private suspend fun compressFileInternal(currentFile: VirtualFile) {
         val file = File(currentFile.path)
         val fileBytes = file.readBytes()
         // Compression Level 3 as it is default
@@ -55,8 +79,9 @@ class CompressOpenFileAction : AnAction() {
         // Write compressed file to same location as input file
         val compFile = File(file.absolutePath + ".zst")
         compFile.writeBytes(compBytes)
-        // Refresh to make result visible faster
-        ApplicationManager.getApplication().invokeLater {
+        // Dispatch onto EDT for UI refresh
+        withContext(Dispatchers.EDT){
+            // Refresh to make result visible faster
             LocalFileSystem.getInstance().refreshAndFindFileByPath(compFile.absolutePath)
         }
     }
